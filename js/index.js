dc215ad626a16df8bafb89557c68131ce37b3214
Leonardo <leon.peralta123456774@gmail.com>
2023-01-11 18:43:46 -0300

    Mundo.svg

--- js/index.js
+++ js/index.js
@@ -3,8 +3,4 @@ const buttons = document.querySelectorAll('.sidebar-button');
 
 
 // CANVAS
-let canvas = document.getElementById("miCanvas");
-let ctx = canvas.getContext("2d");
-ctx.fillStyle = "black";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
-
+document.querySelector("#map_chart1")
