c673004dc6d6475c1c9d0c96b7359c6e3e6c8482
Leonardo <leon.peralta123456774@gmail.com>
2023-01-11 14:27:09 -0300

    Agregando y probando Canvas

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
 // Obtener todos los botones del menú
 const buttons = document.querySelectorAll('.sidebar-button');
 
+
+// CANVAS
+let canvas = document.getElementById("miCanvas");
+let ctx = canvas.getContext("2d");
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
